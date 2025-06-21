0095af836bb1b8d72cf9b8bb07a7571aa8de8a51
Sandip Dhakal <dhakalsandip487@gmail.com>
2025-06-21 23:29:19 +0545

    Few fixes

--- server.js
+++ server.js
@@ -14,12 +14,17 @@ dotenv.config()
 
 const app = express()
 
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true, // Optional: only if you're sending cookies or auth headers
+}));
+
 // Middleware
 // Allow Vercel domain
-app.use(cors({
-  origin: process.env.CORS_ORIGIN,
-  credentials: true,
-}));
+// app.use(cors({
+//   origin: process.env.CORS_ORIGIN || "http://localhost:5173",
+//   credentials: true,
+// }));
 app.use(express.json({ limit: "10mb" }))
 app.use(express.urlencoded({ extended: true, limit: "10mb" }))
 
